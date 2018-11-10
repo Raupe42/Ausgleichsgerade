80ab0fd345b5e12824bc3ed69cfbe11e9b94a00b
RAUPE-SURFACE\jensh <jens_hoecker@t-online.de>
2018-11-10 11:28:51 +0100

    datenHandling audgelagert

--- Programm/Ausgleichsgrade/Main.c
+++ Programm/Ausgleichsgrade/Main.c
@@ -18,7 +18,7 @@ Dies ist die Hauptdatei ...
 #define Ü \x9A
 #define ß \xE1
 
-#define SPEICHERRESERVE 5	//freie Messwertfelder vor realloziieren
+
 
 
 
@@ -52,9 +52,13 @@ gcc -dM -E - </dev/null
 
 	//von eigenen Modulen
 #include "help.h"
+#include "datenHandling.h"
 
 //globale typedefs
 
+
+#ifndef _messdaten_t
+	#define _messdaten_t
 	/*
 	x,y Koordinaten des Messwertes
 	val für Gültigkeit ('1': gültig; '0' oder andere: ungültig
@@ -66,15 +70,16 @@ typedef struct struct_messwert {
 } messwert_t;
 
 typedef struct struct_messreihe {
-	messwert_t (*messreihe)[];
+	messwert_t(*messreihe)[];
 	long anzahlMesswerte;
 	long kapazitaetMessreihe;
 } messreihe_t;
 
+#endif // !_messdaten_t
+
 //Prototypen
 int menue();
-int messreiheAllocate(long neueAnzahl, messreihe_t *p_messreihe);
-int messreihePruefen(messreihe_t *p_messreihe);
+
 
 
 //MAIN
@@ -169,95 +174,4 @@ int menue()
 	} while (retVal == -1);
 
 	return retVal;
-}
-
-/* 
-
-Parameter:
-anzahl: Anzahl der messwert_t die aufgenommen werden sollen
-	bei Werten <= 0 wird die bestehnde Messreihe gelöscht
-struct messwert_t messreihe: Zeiger auf die bestehende Messreihe
-	wird NULL übergeben, wird eine neue Messreihe angelegt
-
-
-
-Rückgabewert: int
-Status des Aufrufs
--1: Fehler
-0: Messreihe leer, kein Fehler
-1: messreihe neu initialisiert
-2: speicher realloziiert
-*/
-int messreiheAllocate(long neueAnzahl, messreihe_t *p_messreihe)
-
-{
-	//lokal für lesabrkeit
-	messwert_t *messreihe = p_messreihe->messreihe;
-	
-	int retVal = - 1;
-
-	if (messreihe == NULL)	//malloc
-	{
-		messreihe = malloc(neueAnzahl * sizeof( messwert_t));
-		retVal= 1;
-	}
-	else if (messreihe && neueAnzahl > 0)	//realloc
-	{
-		messreihe = (messwert_t*) realloc(messreihe, neueAnzahl * sizeof(messwert_t));
-		retVal = 2;
-	}
-	else if (messreihe && neueAnzahl <= 0)	//free
-	{
-		free(messreihe);
-		retVal = 0;
-	}
-	else
-		return -1;
-	if (!messreihe)
-		retVal = -1;
-	printf("%p", messreihe);
-
-	//*p_messreihe = messreihe;
-
-	//lokal zrück
-	p_messreihe->messreihe = messreihe;
-	return retVal;
-}
-
-/*Verwaltung des Messreihenspeichers
-der benötigte Speicherplatz im Hauptspeicher soll
-relativ optimal dimensioniert werden
-
-Parameter:
-Alle Werte werden als InPlaceSubstitution ggf. geändert
-.
-.
-.
-
-
-Rückgabewert Status:
-0: kein Fehler
-*/
-
-int messreihePruefen(messreihe_t *p_messreihe)
-{
-	//lokaldaten zur lesbarkeit
-	long anzahlMesswerte = p_messreihe->anzahlMesswerte;
-	long kapazitaetMessreihe = p_messreihe->kapazitaetMessreihe;
-	//prüfe Messreihe
-	if (kapazitaetMessreihe < anzahlMesswerte)
-	{
-		int state = messreiheAllocate(anzahlMesswerte + SPEICHERRESERVE, p_messreihe);
-		kapazitaetMessreihe = anzahlMesswerte + SPEICHERRESERVE;
-	}
-	else if (kapazitaetMessreihe > anzahlMesswerte * 1.1)		//TODO: Prüfe, ob keine gültigen Messwerte verworfen werden
-	{
-		int state = messreiheAllocate(anzahlMesswerte + SPEICHERRESERVE, p_messreihe);
-		kapazitaetMessreihe = anzahlMesswerte + SPEICHERRESERVE;
-	}
-
-	//rückschreiben der lokaldaten
-	p_messreihe->anzahlMesswerte = anzahlMesswerte;
-	p_messreihe->kapazitaetMessreihe = kapazitaetMessreihe;
-	return 0;
 }
