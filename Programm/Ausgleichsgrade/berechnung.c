6f04894558d4c17920880671eb3c2ac6a012f262
RAUPE-PC\jensh <jens_hoecker@t-online.de>
2018-12-01 06:57:38 +0100

    berechnung vorbereitet

--- Programm/Ausgleichsgrade/berechnung.c
+++ Programm/Ausgleichsgrade/berechnung.c
@@ -41,3 +41,40 @@ double berechneSteigung(messreihe_t* p_messreihe)
 {
 
 }
+
+long double berechneNenner(messreihe_t* p_messreihe)
+{
+
+}
+
+long double berechneSumme(messreihe_t* p_messreihe, double (*sumFunk)(messwert_t* p_messwert))
+{
+	int i;
+	long double summe = 0;
+	for (i = 0; i < p_messreihe->anzahlMesswerte; i++)
+	{
+
+	}
+}
+
+
+
+double sumX(messwert_t* p_messwert)
+{
+	return p_messwert->x;
+}
+
+double sumY(messwert_t* p_messwert)
+{
+	return p_messwert->y;
+}
+
+double sumXpow2(messwert_t* p_messwert)
+{
+	return (p_messwert->x * p_messwert->x);
+}
+
+double sumXY(messwert_t* p_messwert)
+{
+	return (p_messwert->x * p_messwert->y);
+}
