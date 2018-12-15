bc3ddba9f39f42a0349db5aa409ab6d69637417a
RAUPE-PC\jensh <jens_hoecker@t-online.de>
2018-12-15 03:17:40 +0100

    letzte Optimierungen, neue Testdaten (in einer RAR - 160MB werden zu 8MB)

--- Programm/Ausgleichsgrade/help.c
+++ Programm/Ausgleichsgrade/help.c
@@ -24,7 +24,19 @@ int main(void)
 void printHelp()
 {
 	system(CLS);
-	printf("hier wird die Hilfe stehen... \n \n \n \n");
+	printf("    *   *  * * Ausgleichsgeraderechner * *  *   *\n\n\n");
+	printf("Dies ist der Ausgleichsgeradenrechner.\n");
+	printf("Es können 2-Dimensionale Datensätze eingegeben, angeschaut\n");
+	printf("und ausgweretet werden.\n\n");
+	printf("Die Bedienung erfolgt ausschlie"str(_sz)"lich "str(_ue)"ber ZUiffern.\n");
+	printf("Bedienungshinweise werden im oberen Bereich der Konsole gegben.\n");
+	printf("Bei der Eingabe ist ein ',' das Zeichen f"str(_ue)"r Dezimalzahlen.\n");
+	printf("Die Datenmanipulation besitzt einige Unterfunktionen, die jeweils durch\n");
+	printf("die Eingabe von \"-1\" zu beenden sind.\n");
+	printf("Der Im- und Export von eigenen sowie fremden Dateien (C74A Standard vorrausgesetzt)\n");
+	printf("ist m"str(_oe)"glich.\n\n");
+	printf("Das Entwicklerteam des Ausgleichsgeradenrechners w"str(_ue)"nscht frohes schaffen.\n");
+
 
 	printf("Beliebige Taste dr"str(_ue)"cken");
 	getchar();
